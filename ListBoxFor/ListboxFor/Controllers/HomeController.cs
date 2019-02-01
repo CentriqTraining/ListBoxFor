5719e34cb343215a59160e82a937b80f9f8bc4fc
Mike Rissen <mrissen@centriq.com>
2019-02-01 08:34:08 -0600

    Initial commit

---
+++ ListBoxFor/ListboxFor/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication56.Models;
+
+namespace WebApplication56.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //var equip = db.Equipment
+            //    .where(e => e.blah == something)
+            //    .select(e => new { e.name });
+            var items = new SelectList(
+                new List<string>() { "KS", "TX", "MO", "CO", "NE", "OK" });
+            ViewBag.Items = items;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(My_DTO data)
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ListBoxFor/ListboxFor/Models/My_DTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication56.Models
+{
+    public class My_DTO
+    {
+        public string[] Selections { get; set; }
+        // normal stuff
+    }
+}
